5a84cfb38820d883c7376feeae6b295517847cc4
yinn_x <1826630034@qq.com>
2022-10-16 11:08:15 +0800

    Initial Commit

---
+++ programs/AddNumber.java
@@ -0,0 +1,23 @@
+public class AddNumber {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int n =0;
+        String h=5.2+"horse";
+        while(n<=10){
+            System.out.print(n+" \n");
+            System.out.print(h);
+            n+=1;
+        }
+    }
+
+    public AddNumber() {
+    }
+
+    @Override
+    public String toString() {
+        return "AddNumber []";
+    }
+    
+}
