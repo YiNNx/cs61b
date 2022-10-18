bf4ebe6f3a636ec183208d8386dbb19c43fd8143
yinn_x <1826630034@qq.com>
2022-10-18 23:53:39 +0800

    feat: start proj1

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,53 @@
+public class ArrayDeque<T> {
+
+    T[] items;
+    int size;
+
+    public ArrayDeque(){
+
+    }
+
+
+    // Adds an item of type T to the front of the deque.
+    public void addLast(T item) {
+
+    }
+
+    // Adds an item of type T to the back of the deque.
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // Returns true if deque is empty, false otherwise.
+    public int size() {
+        return 0;
+    }
+
+    // Returns the number of items in the deque.
+    public void printDeque() {
+
+    }
+
+    // Prints the items in the deque from first to last, separated by a space.
+    public T removeFirst() {
+        return null;
+    }
+
+    // Removes and returns the item at the front of the deque. If no such item
+    // exists, returns null.
+    public T removeLast() {
+        return null;
+    }
+
+    // Removes and returns the item at the back of the deque. If no such item
+    // exists, returns null.
+    public T get(int index) {
+        return null;
+    }
+
+    // Gets the item at the given index, where 0 is the front, 1 is the next item,
+    // and so forth. If no such item exists, returns null. Must not alter the deque!
+    public void addFirst(T item) {
+
+    }
+}
